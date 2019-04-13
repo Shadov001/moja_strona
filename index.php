0227a0393d48cd191184846ec50097432aed9aca
Shadov001 <marcinjamroz4@wp.pl>
2019-04-13 17:05:28 +0200

    My first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+ <title> Login </title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+    <div class="loginbox">
+    <div class="loginbox-content">
+        <div class="avatar">
+            <img src="avatar.jpg" />
+         <div class="form_login">   
+            <form method="POST" id="form_login">
+        <p>
+            <input type="text" id="username" placeholder="Login" />
+        </p>
+        <p>
+            <input type="password" id="password" placeholder="Hasło" />
+        </p>
+        <p>
+            <button id="submitbutton" type="button">Zaloguj</button>
+        </p>
+    </form>
+</div>
+       
+        </div>      
+       </div>
+    </div>
+
+</body>
+</head>
+</html>
